cd184f8a9fc388c8b902f19d7f2f0c83385692aa
Lattie <gracemszhao@gmail.com>
2025-01-25 14:43:34 -0500

    Added Framework for Pages (navbar + router) - GZ

---
+++ myjournal/src/Error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+      <h1>ERROR!</h1>
+      <h2>So Sorry! This page does not exist!</h2>
+      <p>Go Back Home?</p>
+    </div>
+  )
+}
+
+export default Error
---
+++ myjournal/src/StressRed/StressRed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StressRed = () => {
+  return (
+    <div>
+      <p>Stress Redudction Page</p>
+    </div>
+  )
+}
+
+export default StressRed
