d26a1bcaa052cfee59fb1fdd49b09a8bc44c9dc0
tanvi <tanvigarg.nj@gmail.com>
2025-01-25 22:37:30 -0500

    adding to-do TG

--- myjournal/src/index.js
+++ myjournal/src/index.js
@@ -9,7 +9,7 @@ import Productivity from './Productivity/Productivity';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Productivity />
+    <App />
   </React.StrictMode>
 );
 
